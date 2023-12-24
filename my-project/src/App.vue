047b847756e5c9ea6a93641c68bc94c818f0fa5c
Peterparker1409 <khoabph99@gmail.com>
2023-12-24 17:30:24 +0700

    vue

---
+++ my-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>chào</h1>
+  <h3>{{ title }}</h3>
+  <button class="rounded-full p-3 border-inherit bg-slate-600 " @click="addNumber">+</button>
+  <b class="font-bold">{{ count }}</b>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello world", // added comma here
+      count: 0,
+    };
+  },
+  methods: { // corrected from methods() to methods:
+    addNumber() {
+      this.count--
+    },
+  },
+};
+</script>
